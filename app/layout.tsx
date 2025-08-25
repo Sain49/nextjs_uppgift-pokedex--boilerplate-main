2f05744295eb9717df0849907fd80bd33cc33e77
Arif.A <arif.sain@gmail.com>
2025-08-25 12:01:31 +0200

    Add Footer component with styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
 };
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
@@ -33,6 +34,7 @@ export default function RootLayout({
         </header> */}
         <Header />
         <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
-        <div>
+    <footer className="bg-gray-800 text-white py-8">
+      <div className="container flex flex-col items-center mx-auto px-4 text-center">
+        <div className="flex justify-center items-center gap-2">
           <Image src="/Logo.png" alt="Pokedex logo" width={40} height={40} />
-          <span>Pokédex</span>
+          <span className="text-2x1 font-bold">Pokédex</span>
         </div>
-        <p>Explore the world of Pokemon</p>
-        <div>
+        <p className="mt-4">Explore the world of Pokemon</p>
+        <div className="flex justify-center gap-4 mt-4">
           <Link
             href="https://www.facebook.com"
             target="_blank"
