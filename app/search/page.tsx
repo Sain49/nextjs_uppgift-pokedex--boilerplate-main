c70d7bdacef284b11622edf001b561f7112b771d
Arif.A <arif.sain@gmail.com>
2025-09-02 12:00:42 +0200

    Refactor Pokedex component to include default Pokemon type and enhance data fetching

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,16 +1,25 @@
-export default function Pokedex({ pokemomType }: { pokemomType: string }) {
+import PokemonGrid from "@/components/PokemonGrid";
+import { getPokemonByType } from "@/lib/api";
+
+export default async function Pokedex({
+  pokemomType = "normal",
+}: {
+  pokemomType: string;
+}) {
+  const pokemonsByType = (await getPokemonByType(pokemomType)).slice(0, 20); // limits to first 20 items
+  console.log(pokemonsByType.map((pokemon: any) => pokemon));
   return (
     <main className="flex flex-col flex-grow">
       <section className="flex flex-col items-center gap-4 bg-gradient-to-br [background-image:linear-gradient(-10deg,_#C97FE4,_#AECDF6)] p-14">
         <h1 className="text-center mt-14 text-8xl font-extrabold text-transparent bg-gradient-to-r from-purple-800 to-blue-800 [background-clip:text]">
           Pokemon Type {pokemomType}
         </h1>
-        <p className="text-center text-white text-xl">
-          Here you can find all the {pokemomType} type of Pokemon.
-        </p>
-        // List of Pokemon by type
-        <div></div>
       </section>
+      {/* List of Pokemon by type */}
+      <PokemonGrid
+        pokemonsList={pokemonsByType}
+        listTitle={`List of ${pokemomType} type Pokemons`}
+      />
     </main>
   );
 }
