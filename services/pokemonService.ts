7bb197fee5a1c97335642ed6fd1c5e96fd3ffdac
Arif.A <arif.sain@gmail.com>
2025-09-05 11:25:07 +0200

    Add PokemonService class to manage Pokémon data retrieval and searching

---
+++ services/pokemonService.ts
@@ -0,0 +1,27 @@
+// services/pokemonService.ts
+import { getPokemon, getPokemonList, getPokemonByType } from "@/lib/api";
+import { getRandomPokemons } from "@/lib/util";
+import { Pokemon, PokemonType } from "@/lib/interfaces";
+
+const pokemonTypeLimits = 20;
+const pokemonFeauturedCount = 4;
+
+export class PokemonService {
+  static async getFeaturedPokemon(): Promise<PokemonType[]> {
+    const pokemonList = await getPokemonList();
+    return getRandomPokemons(pokemonList, pokemonFeaturedCount);
+  }
+
+  static async searchPokemon(nameOrId: string): Promise<Pokemon> {
+    return getPokemon(nameOrId);
+  }
+
+  static async getPokemonsByType(type: string): Promise<PokemonType[]> {
+    const pokemonsByType = await getPokemonByType(type);
+    return pokemonsByType.slice(0, pokemonTypeLimits);
+  }
+
+  static async getAllPokemonForRandom(): Promise<PokemonType[]> {
+    return getPokemonList();
+  }
+}
