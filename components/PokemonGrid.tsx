9add4ba96b807491593650e5f50c4659d16268d8
Arif.A <arif.sain@gmail.com>
2025-09-05 09:14:42 +0200

    Refactor PokemonGrid component to use title prop and update to PokemonCardContainer for rendering

--- components/PokemonGrid.tsx
+++ components/PokemonGrid.tsx
@@ -1,20 +1,25 @@
-import PokemonCard from "@/components/PokemonCard";
+import PokemonCardContainer from "@/components/PokemonCardContainer";
+import { PokemonType } from "@/lib/interfaces";
+
+interface PokemonGridProps {
+  pokemonsList: PokemonType[];
+  title: string;
+  className?: string;
+}
 
 export default function PokemonGrid({
   pokemonsList,
-  listTitle,
-}: {
-  pokemonsList: any[];
-  listTitle: string;
-}) {
+  title,
+  className = "p-8 bg-gradient-to-r from-purple-100 to-blue-100",
+}: PokemonGridProps) {
   return (
-    <section className="p-8 bg-gradient-to-r from-purple-100 to-blue-100">
+    <section className={className}>
       <h2 className="text-3xl font-bold text-center mb-8 font-stretch-expanded">
-        {listTitle}
+        {title}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-4xl mx-auto">
-        {pokemonsList.map((pokemon: any) => (
-          <PokemonCard key={pokemon.name} pokemon={pokemon} />
+        {pokemonsList.map((pokemon) => (
+          <PokemonCardContainer key={pokemon.name} pokemon={pokemon} />
         ))}
       </div>
     </section>
